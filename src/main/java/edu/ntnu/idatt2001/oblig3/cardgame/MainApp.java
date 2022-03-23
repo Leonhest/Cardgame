6aacca777a66182a04a5d4a40ff89bbb2a44914d
Leonhest <leon.hesthaug@gmail.com>
2022-03-23 21:14:26 +0100

    fix: small fixes, remove module_info

--- src/main/java/edu/ntnu/idatt2001/oblig3/cardgame/MainApp.java
+++ src/main/java/edu/ntnu/idatt2001/oblig3/cardgame/MainApp.java
@@ -14,7 +14,7 @@ public class MainApp extends Application{
 
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/edu.ntnu.idatt2001.oblig3.cardgame/cardGUI.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/edu/ntnu/idatt2001/oblig3/cardgame/cardGUI.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Card game");
         stage.setScene(scene);
--- src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module cardgame {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens edu.ntnu.idatt2001.oblig3.cardgame to javafx.fxml;
-    exports edu.ntnu.idatt2001.oblig3.cardgame;
-}
